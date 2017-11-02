987f54f77cbe69e07439ca3ba633e41696413101
Ubuntu <ubuntu@ip-172-30-11-109.ec2.internal>
2017-11-02 09:49:34 +0000

    Update the environment starter to use the current directory.

--- exercise_platform/start_containers.sh
+++ exercise_platform/start_containers.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker run --name ceudsd-mysql --restart unless-stopped -p 3306:3306 -v /home/ubuntu/mysql/mysql-files:/var/lib/mysql-files/ -v /home/ubuntu/mysql/db:/var/lib/mysql -v /home/ubuntu/mysql/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=ceudsd666 -d mysql/mysql-server:5.7
-docker run -p 80:8080 --restart unless-stopped -v /home/ubuntu/zeppelin/conf:/zeppelin/conf -v /home/ubuntu/zeppelin/logs:/logs -v /home/ubuntu/zeppelin/interpreter/mysql:/zeppelin/interpreter/mysql -v /home/ubuntu/zeppelin/notebook:/notebook -e ZEPPELIN_LOG_DIR='/logs' -e ZEPPELIN_NOTEBOOK_DIR='/notebook' --link ceudsd-mysql:mysql --name zeppelin -d apache/zeppelin:0.7.3
+BASE_DIR=`pwd`
+
+docker run --name ceudsd-mysql --restart unless-stopped -p 3306:3306 -v $BASE_DIR/mysql/mysql-files:/var/lib/mysql-files/ -v $BASE_DIR/mysql/db:/var/lib/mysql -v $BASE_DIR/mysql/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=ceudsd666 -d mysql/mysql-server:5.7
+docker run -p 80:8080 --restart unless-stopped -v $BASE_DIR/zeppelin/conf:/zeppelin/conf -v $BASE_DIR/zeppelin/logs:/logs -v $BASE_DIR/zeppelin/interpreter/mysql:/zeppelin/interpreter/mysql -v $BASE_DIR/zeppelin/notebook:/notebook -e ZEPPELIN_LOG_DIR='/logs' -e ZEPPELIN_NOTEBOOK_DIR='/notebook' --link ceudsd-mysql:mysql --name zeppelin -d apache/zeppelin:0.7.3
